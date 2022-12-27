e2e92a441ded389b00c216282ae7f05c71363b2b
Selcan Gulec <selcan.gulec@gmail.com>
2022-12-27 11:56:34 -0600

    Update Letterbox_Paint_Squad.py

--- 7kyuKataPractices/Letterbox_Paint_Squad.py
+++ 7kyuKataPractices/Letterbox_Paint_Squad.py
@@ -1,2 +1,24 @@
-def multiply(a, b):
-    return (a * b)
+def paint_letterboxes(start, finish):
+    number_dic = {}
+    arr = []
+
+    for i in range(start, finish + 1):
+        splitting = list(str(i))
+        for j in splitting:
+            arr.append(j)
+
+    for i in arr:
+        if int(i) in number_dic:
+            number_dic[int(i)] += 1
+        else:
+            number_dic[int(i)] = 1
+
+    ans = []
+
+    for i in range(0, 10):
+        if i in number_dic:
+            ans.append(number_dic[i])
+        else:
+            ans.append(0)
+
+    return ans
